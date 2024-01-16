c3247aa43b49590a164ae5706c8ec8c4a2491ed1
dasoya <66422476+dasoya@users.noreply.github.com>
2024-01-16 18:03:46 +0900

    2 주차 실습 코드 WeSplit

--- iOS/WeSplit.swift
+++ iOS/WeSplit.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct WeSplit: View {
+    
+    @State var checkAmount = 0.0
+    @State var numberOfPeople = 0
+    @State var tipPercentage = 0
+    
+    var totalAmount : Double {
+        let tipValue = checkAmount/100 * Double(tipPercentage)
+        
+        return checkAmount + tipValue
+    }
+    
+    var totalPerPerson : Double {
+        return totalAmount/Double(numberOfPeople+2)
+    }
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            Form{
+            
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: "KRW"))
+                    
+                    Picker("Number of People",selection: $numberOfPeople){
+                        ForEach(2..<51){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section{
+                    Picker("Tip percentage",selection: $tipPercentage){
+                        
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0,format:.percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section{
+                    Text(totalAmount,format:.currency(code: "KRW"))
+                        .foregroundColor(tipPercentage == 0 ? .red : .black)
+                }
+                
+                Section{
+                    Text(totalPerPerson,format:.currency(code: "KRW"))
+                }
+                
+            }.navigationTitle("WeSplit")
+        }
     }
 }
 
--- iOS/iOSApp.swift
+++ iOS/iOSApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           // ContentView()
+            WeSplit()
         }
     }
 }
