e91720bdaae9a75e44dd05be01eb3eb4bac713f9
dasoya <66422476+dasoya@users.noreply.github.com>
2024-01-16 12:03:04 +0900

    init project

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tapCount = 0
+    @State var name = ""
+    
+    var names = ["다소","다솜","길동"]
+    @State var selectedName = "다소"
+    var cake : Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+     
+       NavigationStack{
+            
+           
+            Form{
+                Section{
+                    Button("Tap Count: \(tapCount)"){
+                        tapCount += 1
+                    }
+                }
+                
+                Section{
+                    TextField("Enter your name",text: $name)
+                    Text("your name is \(name)")
+                }
+                
+                Section{
+                    
+//                    ForEach(0..<6){ number in
+//                        Text("\(number)")
+//                    }
+                    
+                    Picker("Select your name", selection: $selectedName){
+                        ForEach(names, id: \.self){
+                            Text("\($0)")
+                        }
+                    }
+                    
+            
+                }
+            }.navigationTitle("iOS")
+       }
     }
 }
 
---
+++ iOS/WeSplit.swift
@@ -0,0 +1,18 @@
+//
+//  WeSplit.swift
+//  iOS
+//
+//  Created by dasoya on 1/16/24.
+//
+
+import SwiftUI
+
+struct WeSplit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeSplit()
+}
